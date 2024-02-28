c7d80d3f0f7de93244ef50493e8867e8f3f16f85
shrfrn <shrfrn@gmail.com>
2024-02-28 13:11:07 +0200

    Make getEl return a custom JQuery-like object

--- js/my-jquery.js
+++ js/my-jquery.js
@@ -5,25 +5,30 @@ window.onload = onInit
 function onInit() {
     console.log('Hi')
 
-    const elBtn = document.querySelector('button')
-    elBtn.addEventListener('click', onBtnClick)
+    const $elBtn = getEl('button')
+    $elBtn.on('click', onBtnClick)
 }
 
 function onBtnClick() {
-    const elHeading = getEl('h1')
-    const elSubHeading = getEl('h2')
-    const elYear = getEl('footer span')
+    const $elHeading = getEl('h1')
+    const $elSubHeading = getEl('h2')
+    const $elYear = getEl('footer span')
 
-    elHeading.classList.add('highlight')
-    setTimeout(() => elHeading.classList.remove('highlight'), 2000)
+    $elHeading.addClass('highlight')
+    setTimeout(() => $elHeading.removeClass('highlight'), 2000)
 
-    elYear.classList.add('highlight')
-    setTimeout(() => elYear.classList.remove('highlight'), 2000)
+    $elYear.addClass('highlight')
+    setTimeout(() => $elYear.removeClass('highlight'), 2000)
 
-    elSubHeading.innerText = 'Welcome to My - JQuery!'
+    $elSubHeading.text('Welcome to My - JQuery!')
 }
 
 function getEl(selector) {
     const el = document.querySelector(selector)
-    return el
+    return {
+        addClass: cls => el.classList.add(cls),
+        removeClass: cls => el.classList.remove(cls),
+        text: txt => el.innerText = txt,
+        on: (event, cb) => el.addEventListener(event, cb),
+    }
 }
